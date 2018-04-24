52b49f0c2ee85f59bbd610ea79d825c2a9c8521c
Underglaze-Blue <lepapillonangel@gmail.com>
2018-04-24 16:11:28 +0800

    vue-ssr

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div id="cover"></div>
+    <m-header></m-header>
+    <todo></todo>
+    <m-footer></m-footer>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import MHeader from './todo/header.vue'
+import MFooter from './todo/footer.jsx'
+import todo from './todo/todo.vue'
+
+export default {
+  data () {
+    return {
+
+    };
+  },
+  components: {
+    MHeader,
+    MFooter,
+    todo
+  }
+};
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus" scoped>
+#app
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+  #cover
+    position absolute
+    left 0
+    top 0
+    right 0
+    bottom 0
+    background-color #999
+    opacity 0.9
+    z-index -1
+</style>
